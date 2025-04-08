b03e240e306cf6353682cdf056fbb793e4fe0e26
mimi <62527898+mimi-0506@users.noreply.github.com>
2025-04-08 13:10:27 +0900

    style: app에 warpper 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,14 @@ import GlobalStyle from "./GlobalStyle";
 import { authAtom } from "./atoms/memoAtom";
 import Auth from "./components/Auth";
 import Header from "./components/Header";
+import styled from "@emotion/styled";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: start;
+`;
 
 function App() {
   const auth = useAtomValue(authAtom);
@@ -11,8 +19,10 @@ function App() {
   return (
     <>
       <GlobalStyle />
-      <Header />
-      {auth ? <Dashboard /> : <Auth />}
+      <Wrapper>
+        <Header />
+        {auth ? <Dashboard /> : <Auth />}
+      </Wrapper>
     </>
   );
 }
