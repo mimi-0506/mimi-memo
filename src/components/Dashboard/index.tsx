c236d3f6fdef41c9084e45991033932a81de7c87
mimi <62527898+mimi-0506@users.noreply.github.com>
2025-04-15 22:34:26 +0900

    feat: 날짜 클릭시 맨 위로 이동 & 해당 날짜로 바뀜

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,23 +1,22 @@
 import Memo from "./Memo";
 
-import { fillMissingDates } from "../../utils/dateUtils";
 import useLoadMemos from "../../hook/useLoadMemos";
 import useSaveMemos from "../../hook/useSaveMemos";
 import styled from "@emotion/styled";
-import { useAtomValue } from "jotai";
-import { memosAtom } from "../../atoms/memoAtom";
 import { useAtomValue, useSetAtom } from "jotai";
 import { dateAtom, deleteEmptyAtom, memosAtom } from "../../atoms/memoAtom";
 import TextInput from "./TextInput";
 import DaySelector from "./DaySelector";
 import useLoadBounds from "../../hook/useLoadBounds";
 import useSaveBounds from "../../hook/useSaveBounds";
+import { useRef } from "react";
 
 export const Wrapper = styled.div`
   padding: 20px;
   width: 100%;
   height: 100%;
   overflow-y: auto;
+  position: relative;
 
   scrollbar-width: thin; /* Firefox */
   scrollbar-color: rgba(255, 255, 255, 0.3) transparent;
@@ -65,25 +64,51 @@ const MemoList = styled.div`
   justify-content: center;
 `;
 
+const ButtonArea = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const DateButton = styled.button``;
+
 export default function Dashboard() {
   const memos = useAtomValue(memosAtom);
   const setDate = useSetAtom(dateAtom);
   const deleteEmpty = useSetAtom(deleteEmptyAtom);
+  const wrapperRef = useRef<HTMLDivElement>(null);
 
   useLoadMemos();
   useSaveMemos();
   useLoadBounds();
   useSaveBounds();
 
+  const handleDateClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const date = e.currentTarget.dataset.date;
+
+    if (date && wrapperRef.current) {
+      setDate(date);
+      console.log(date);
+      wrapperRef.current.scrollTo({ top: 0, behavior: "smooth" });
+    }
+  };
+
   return (
-    <Wrapper>
+    <Wrapper ref={wrapperRef}>
+      <ButtonArea>
+        <DaySelector />
+        <button onClick={deleteEmpty}>빈배열 삭제</button>
+      </ButtonArea>
       <TextInput />
       <MemoList>
         {Array.from(memos.entries())
           .sort()
           .map(([date, memos]) => (
             <MemoGroup key={date}>
+              <DateButton data-date={date} onClick={handleDateClick}>
                 {date}
+              </DateButton>
               {memos.map((memo) => (
                 <Memo key={memo.id} memo={memo} />
               ))}
